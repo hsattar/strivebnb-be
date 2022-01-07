c3842e9a08b0120963a4ff0648b1f56834fb1a0d
hsattar <39243545+hsattar@users.noreply.github.com>
2022-01-07 15:05:41 +0000

    connected to db

---
+++ src/db/connection.js
@@ -0,0 +1,17 @@
+import Sequelize from "sequelize"
+
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: DB_DIALECT
+})
+
+export const testDBConnection = async () => {
+    try {
+        await sequelize.authenticate({ logging: false })
+        console.log('DB Connected')
+    } catch (error) {
+        console.error('DB Failed', error)
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { testDBConnection } from "./db/connection.js"
 
 const server = express()
 const { PORT } = process.env
@@ -8,6 +9,9 @@ const port = PORT || 3001
 server.use(express.json())
 server.use(cors())
 
+server.get('/', (req, res) => res.send('OK'))
+
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
+    testDBConnection()
 })
