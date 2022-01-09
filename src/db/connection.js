90275dc2204edbc71361192259c75642aaf91f67
hsattar <39243545+hsattar@users.noreply.github.com>
2022-01-09 17:26:27 +0000

    added db info for remote db

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,19 @@
 import Sequelize from "sequelize"
 
-const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DIALECT, DATABASE_URL } = process.env
 
-export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: DB_DIALECT
+// export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+//     host: DB_HOST,
+//     dialect: DB_DIALECT
+// })
+
+export const sequelize = new Sequelize(DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+    }
 })
 
 export const testDBConnection = async () => {
